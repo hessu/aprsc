1959427bea43bc0a34fe5d5b880bfbc1fe58a1c4
Heikki Hannikainen <hessu@hes.iki.fi>
2008-03-01 09:55:33 +0000

    Documented rwlock origins (Butenhof book)
    git-svn-id: http://repo.ham.fi/svn/aprsc/trunk@32 3ce903b1-3385-4e86-93cd-f9a4a239f7ac

--- src/rwlock.h
+++ src/rwlock.h
@@ -1,22 +1,7 @@
 /*
- *	aprsc
- *
- *	(c) Heikki Hannikainen, OH7LZB <hessu@hes.iki.fi>
- *
- *    This program is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 2 of the License, or
- *    (at your option) any later version.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with this program; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *	
+ *  This code is from the book "Programming with POSIX Threads", by
+ *  David R. Butenhof. Appears in modified and GPL'ed form in at least
+ *  the Bacula sources.
  */
 
 #ifndef RWLOCK_H
@@ -72,7 +57,7 @@ typedef struct rwlock_tag {
 #define RWLOCK_VALID    0xfacade
 
 /*
- * Support static initialization of barriers
+ * Support static initialization of read-write locks.
  */
 #define RWL_INITIALIZER \
     {PTHREAD_MUTEX_INITIALIZER, PTHREAD_COND_INITIALIZER, \

80a20cbfdf86202cd73b0392cb848f5c921f725b
Heikki Hannikainen <hessu@hes.iki.fi>
2008-03-22 10:34:57 +0000

    Remove references to hemserv, replace with aprsc
    git-svn-id: http://repo.ham.fi/svn/aprsc/trunk@214 3ce903b1-3385-4e86-93cd-f9a4a239f7ac

--- src/hlog.c
+++ src/hlog.c
@@ -48,13 +48,13 @@ int log_level = LOG_INFO;	/* Logging level */
 int log_facility = LOG_LOCAL1;	/* Logging facility */
 char *log_name = NULL;		/* Logging name */
 
-char log_basename[] = "hemserv.log";
+char log_basename[] = "aprsc.log";
 char *log_dir = NULL;		/* Access log directory */
 char *log_fname = NULL;		/* Access log file name */
 int log_file = -1;		/* If logging to a file, the file name */
 rwlock_t log_file_lock = RWL_INITIALIZER;
 
-char accesslog_basename[] = "hemserv.access.log";
+char accesslog_basename[] = "aprsc.access.log";
 char *accesslog_dir = NULL;	/* Access log directory */
 char *accesslog_fname = NULL;	/* Access log file name */
 int accesslog_file = -1;	/* Access log fd */
@@ -132,7 +132,7 @@ int open_log(char *name, int reopen)
 		hfree(log_name);
 		
 	if (!(log_name = hstrdup(name))) {
-		fprintf(stderr, "hemserv logger: out of memory!\n");
+		fprintf(stderr, "logger: out of memory!\n");
 		exit(1);
 	}
 	
@@ -148,7 +148,7 @@ int open_log(char *name, int reopen)
 		
 		log_file = open(log_fname, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR|S_IRGRP);
 		if (log_file < 0) {
-			fprintf(stderr, "hemserv logger: Could not open %s: %s\n", log_fname, strerror(errno));
+			fprintf(stderr, "logger: Could not open %s: %s\n", log_fname, strerror(errno));
 			exit(1);
 		}
 	}
@@ -234,17 +234,17 @@ int hlog(int priority, const char *fmt, ...)
 		rwl_rdlock(&log_file_lock);
 		fprintf(stderr, "%4d/%02d/%02d %02d:%02d:%02d.%06d %s[%d:%lu] %s: %s\n",
 			lt.tm_year + 1900, lt.tm_mon + 1, lt.tm_mday, lt.tm_hour, lt.tm_min, lt.tm_sec, (int)tv.tv_usec,
-			(log_name) ? log_name : "hemserv", (int)getpid(), (unsigned long int)pthread_self(), log_levelnames[priority], s);
+			(log_name) ? log_name : "aprsc", (int)getpid(), (unsigned long int)pthread_self(), log_levelnames[priority], s);
 		rwl_rdunlock(&log_file_lock);
 		
 	} else if ((log_dest == L_FILE) && (log_file >= 0)) {
 		len = snprintf(wb, LOG_LEN, "%4d/%02d/%02d %02d:%02d:%02d.%06d %s[%d:%ld] %s: %s\n",
 			       lt.tm_year + 1900, lt.tm_mon + 1, lt.tm_mday, lt.tm_hour, lt.tm_min, lt.tm_sec, (int)tv.tv_usec,
-			       (log_name) ? log_name : "hemserv", (int)getpid(), (unsigned long int)pthread_self(), log_levelnames[priority], s);
+			       (log_name) ? log_name : "aprsc", (int)getpid(), (unsigned long int)pthread_self(), log_levelnames[priority], s);
 		wb[LOG_LEN-1] = 0;
 		rwl_rdlock(&log_file_lock);
 		if ((w = write(log_file, wb, len)) != len)
-			fprintf(stderr, "hemserv logger: Could not write to %s (fd %d): %s\n", log_fname, log_file, strerror(errno));
+			fprintf(stderr, "logger: Could not write to %s (fd %d): %s\n", log_fname, log_file, strerror(errno));
 		rwl_rdunlock(&log_file_lock);
 		
 	} else if (log_dest == L_SYSLOG) {

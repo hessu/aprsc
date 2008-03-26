038d1fd51a4421e5b63fa82566ef4ba2a4084a43
Matti Aarnio <oh2mqk@sral.fi>
2008-03-26 14:51:23 +0000

    cleanup
    git-svn-id: http://repo.ham.fi/svn/aprsc/trunk@260 3ce903b1-3385-4e86-93cd-f9a4a239f7ac

--- src/filter.h
+++ src/filter.h
@@ -34,19 +34,15 @@ extern int  filter_commands(struct worker_t *self, struct client_t *c, const cha
 extern void filter_preprocess_dupefilter(struct pbuf_t *pb);
 extern void filter_postprocess_dupefilter(struct pbuf_t *pb);
 
-extern int  filter_entrycall_insert(struct pbuf_t *pb);
 extern void filter_entrycall_cleanup(void);
 extern void filter_entrycall_atend(void);
 extern int  filter_entrycall_cellgauge;
 extern void filter_entrycall_dump(FILE *fp);
-extern void filter_entrycall_load(FILE *fp);
 
-extern int  filter_wx_insert(struct pbuf_t *pb);
 extern void filter_wx_cleanup(void);
 extern void filter_wx_atend(void);
 extern int  filter_wx_cellgauge;
 extern void filter_wx_dump(FILE *fp);
-extern void filter_wx_load(FILE *fp);
 
 extern float filter_lat2rad(float lat);
 extern float filter_lon2rad(float lon);

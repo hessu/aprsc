f83a099b4d1db7ebfcc719a098337b4b0a6a26ca
Heikki Hannikainen <hessu@hes.iki.fi>
2009-03-10 18:17:17 +0000

    + istest module
    git-svn-id: http://repo.ham.fi/svn/aprsc/trunk@299 3ce903b1-3385-4e86-93cd-f9a4a239f7ac

---
+++ tests/libperl/istest.pm
@@ -0,0 +1,91 @@
+
+package istest;
+
+=head1 NAME
+
+istest - a perl module for doing APRS-IS network server tests.
+
+=cut
+
+use 5.006;
+use strict;
+use warnings;
+use Data::Dumper;
+
+sub txrx($$$$$)
+{
+	my($ok, $i_tx, $i_rx, $tx, $rx) = @_;
+	
+	my $sent = $i_tx->sendline($tx);
+	
+	if (!$sent) {
+		&$ok($sent, 1, "Failed to send line to server: '$tx'");
+		return;
+	}
+	
+	my $received = $i_rx->getline_noncomment();
+	
+	if (!defined $received) {
+		if ($i_rx->{'state'} eq 'connected') {
+			&$ok(1, 0, "Did not receive packet from server (timeout): '$tx'");
+		} else {
+			&$ok(1, 0, "Server connection went down after sending: '$tx'");
+		}
+		return;
+	}
+	
+	if ($received ne $rx) {
+		&$ok($received, $rx, "Server returned wrong line");
+	}
+	
+	&$ok(1, 1, "ok");
+}
+
+sub should_drop($$$$$)
+{
+	my($ok, $i_tx, $i_rx, $tx, $helper) = @_;
+	
+	my $drop_key = 'drop.' . int(rand(1000000));
+	my $sent = $i_tx->sendline($tx . ' ' . $drop_key);
+	
+	if (!$sent) {
+		&$ok($sent, 1, "Failed to send line to server: '$tx'");
+		return;
+	}
+	
+	my $helper_key = 'helper.' . int(rand(1000000));
+	$sent = $i_tx->sendline($helper. ' ' . $helper_key);
+	
+	if (!$sent) {
+		&$ok($sent, 1, "Failed to send helper line to server: '$tx'");
+		return;
+	}
+	
+	my $received = $i_rx->getline_noncomment();
+	
+	if (!defined $received) {
+		if ($i_rx->{'state'} eq 'connected') {
+			&$ok(1, 0, "Did not receive packet from server (timeout): '$tx'");
+		} else {
+			&$ok(1, 0, "Server connection went down after sending: '$tx'");
+		}
+		return;
+	}
+	
+	if ($received =~ /$helper_key/) {
+		&$ok(1, 1, "ok, received helper packet only");
+		return;
+	}
+	
+	if ($received =~ /$drop_key/) {
+		&$ok($received, $helper, "Server did not drop the packet it should have dropped");
+	}
+	
+	# get one more
+	$i_rx->getline_noncomment();
+	
+	&$ok(1, 1, "ok");
+}
+
+
+1;

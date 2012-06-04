04b99655a9f43305da765fc234dc09d8c9cf0473
Heikki Hannikainen <hessu@hes.iki.fi>
2012-06-04 14:41:19 +0000

    istest: more debug output when needed. Support for no_random_helper.
    git-svn-id: http://repo.ham.fi/svn/aprsc/trunk@383 3ce903b1-3385-4e86-93cd-f9a4a239f7ac

--- tests/libperl/istest.pm
+++ tests/libperl/istest.pm
@@ -12,16 +12,27 @@ use strict;
 use warnings;
 use Data::Dumper;
 
+my $debug = 0;
+
 sub txrx($$$$$)
 {
 	my($ok, $i_tx, $i_rx, $tx, $rx) = @_;
 	
+	warn "sending: $tx\n" if ($debug);
 	my $sent = $i_tx->sendline($tx);
 	
+	warn "sent\n" if ($debug);
+	
 	if (!$sent) {
-		&$ok($sent, 1, "Failed to send line to server: '$tx'");
+		&$ok(0, 1, "Failed to send line to server: '$tx'");
 		return;
 	}
+	
+	if ($i_tx->{'state'} ne 'connected') {
+		&$ok(1, 0, "Server TX connection error after sending: '$tx': " . $i_tx->{'error'});
+	}
+	
+	warn "receiving\n" if ($debug);
 	
 	my $received = $i_rx->getline_noncomment();
 	
@@ -29,21 +40,22 @@ sub txrx($$$$$)
 		if ($i_rx->{'state'} eq 'connected') {
 			&$ok(1, 0, "Did not receive packet from server (timeout): '$tx'");
 		} else {
-			&$ok(1, 0, "Server connection went down after sending: '$tx'");
+			&$ok(1, 0, "Server RX connection error after sending: '$tx': " . $i_rx->{'error'});
 		}
 		return;
 	}
 	
 	if ($received ne $rx) {
 		&$ok($received, $rx, "Server returned wrong line");
+		return;
 	}
 	
 	&$ok(1, 1, "ok");
 }
 
-sub should_drop($$$$$;$)
+sub should_drop($$$$$;$$)
 {
-	my($ok, $i_tx, $i_rx, $tx, $helper, $no_random_drop) = @_;
+	my($ok, $i_tx, $i_rx, $tx, $helper, $no_random_drop, $no_random_helper) = @_;
 	
 	my $drop_key = '';
 	$drop_key .= ' drop.' . int(rand(1000000)) if (!$no_random_drop);
@@ -55,7 +67,8 @@ sub should_drop($$$$$;$)
 	}
 	
 	my $helper_key = 'helper.' . int(rand(1000000));
-	my $helper_l = $helper. ' ' . $helper_key;
+	my $helper_l = $helper;
+	$helper_l .= ' ' . $helper_key if (!$no_random_helper);
 	$sent = $i_tx->sendline($helper_l);
 	
 	if (!$sent) {
@@ -74,18 +87,42 @@ sub should_drop($$$$$;$)
 		return;
 	}
 	
-	if ($received =~ /$helper_key/) {
-		&$ok(1, 1, "ok, received helper packet only");
-		return;
-	}
+	my $tx2 = $tx;
+	$tx2 =~ s/([^>]+)>([^,]+),[^:]+(:.*)$/$1>$2$3/;
+	my $hl2 = $helper;
+	$hl2 =~ s/([^>]+)>([^,]+),[^:]+(:.*)$/$1>$2$3/;
+	my $rec2 = $received;
+	$rec2 =~ s/([^>]+)>([^,]+),[^:]+(:.*)$/$1>$2$3/;
 	
-	if ($received =~ /$drop_key/) {
-		&$ok($received, $helper, "Server forwarded packet it should have dropped");
+	if ($no_random_helper) {
+		#warn "exp '$hl2'\n";
+		#warn "got '$rec2'\n";
+		if ($hl2 eq $rec2) {
+			&$ok(1, 1, "ok, received helper packet only");
+			return;
+		}
 	} else {
-		&$ok($received, $helper, "Server returned completely unexpected packet");
+		if ($received =~ /$helper_key/) {
+			&$ok(1, 1, "ok, received helper packet only");
+			return;
+		}
 	}
 	
-	# get one more
+	if ($no_random_drop) {
+		if ($hl2 eq $tx2) {
+			&$ok($received, $helper, "Server forwarded packet it should have dropped");
+		} else {
+			&$ok($received, $helper, "Server returned completely unexpected packet");
+		}
+	} else {
+		if ($received =~ /$drop_key/) {
+			&$ok($received, $helper, "Server forwarded packet it should have dropped");
+		} else {
+			&$ok($received, $helper, "Server returned completely unexpected packet");
+		}
+	}
+	
+	# since we received an extra packet, get one more line to receive the helper
 	$i_rx->getline_noncomment();
 }
 

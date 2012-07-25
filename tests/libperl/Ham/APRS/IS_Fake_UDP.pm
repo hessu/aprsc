db44312240821cfb63634d0402510444a1b30ebe
Heikki Hannikainen <hessu@hes.iki.fi>
2012-07-25 22:03:00 +0000

    Implemented UDP peer group tests
    git-svn-id: http://repo.ham.fi/svn/aprsc/trunk@499 3ce903b1-3385-4e86-93cd-f9a4a239f7ac

---
+++ tests/libperl/Ham/APRS/IS_Fake_UDP.pm
@@ -0,0 +1,143 @@
+
+package Ham::APRS::IS_Fake_UDP;
+
+use 5.006;
+use strict;
+use warnings;
+
+use IO::Handle '_IOFBF';
+use IO::Socket::INET;
+use IO::Select;
+
+use Ham::APRS::IS;
+
+=head1 new(hostport, mycall, optionshash)
+
+Initializes a new Ham::APRS::IS_Fake_UDP listening socket. Takes two mandatory arguments,
+the host:port pair to listen on and the server's callsign.
+
+  my $udp = new Ham::APRS::IS_Fake_UDP('*:12765', 'N0CALL');
+
+=cut
+
+sub new($$$;%)
+{
+	my $that = shift;
+	my $class = ref($that) || $that;
+	my $self = { };
+	bless ($self, $class);
+	
+	my($host_port, $mycall, %options) = @_;
+	
+	$self->{'host_port'} = $host_port;
+	if (defined $mycall) {
+		$self->{'mycall'} = $mycall;
+	} else {
+		$self->{'mycall'} = "FAKE" . sprintf("%d", rand(999));
+	}
+	#$self->{'filter'} = $options{'filter'} if (defined $options{'filter'});
+	
+	#warn "aprspass for $self->{mycall} is $self->{aprspass}\n";
+	
+	$self->{'state'} = 'init';
+	$self->{'error'} = "No errors yet.";
+	
+	return $self;
+}
+
+sub bind_and_listen($)
+{
+	my($self) = @_;
+	
+	my($localaddr, $localport) = split(':', $self->{'host_port'}); # TODO: ipv6...
+	
+	$self->{'usock'} = IO::Socket::INET->new(
+		Proto => 'udp',
+		LocalPort => $localport,
+		LocalAddr => $localaddr,
+		ReuseAddr => 1,
+		ReeusePort => 1);
+	
+	if (!defined($self->{'usock'})) {
+		$self->{'error'} = "Failed to bind an UDP client socket: $@";
+		return 0;
+        }
+        
+        warn "bound udp port $localaddr $localport\n";
+        
+        $self->{'state'} = 'connected';
+        return 1;
+}
+
+sub set_destination($$)
+{
+        my($self, $hostport) = @_;
+        
+	my($host, $port) = split(':', $hostport); # TODO: ipv6...
+	
+	my $hisiaddr = inet_aton($host) || die "unknown host";
+	my $hispaddr = sockaddr_in($port, $hisiaddr);
+	
+	$self->{'dest'} = $hispaddr;
+}
+
+sub unbind($)
+{
+	my($self) = @_;
+	
+	undef $self->{'usock'};
+}
+
+sub getline($;$)
+{
+	my($self, $timeout) = @_;
+	
+	return undef if ($self->{'state'} ne 'connected');
+	
+	$timeout = 5 if (!defined $timeout);
+	
+	my $end_t = time() + $timeout;
+	my $sock = $self->{'usock'};
+	
+	while (1) {
+		my($rin, $rout, $ein, $eout) = ('', '', '', '');
+		vec($rin, fileno($self->{'usock'}), 1) = 1;
+		my $nfound = select($rout = $rin, undef, $eout = $ein, $timeout);
+		
+		if ($nfound) {
+			my $rbuf;
+			if (($rout & $rin) eq $rin) {
+				warn "getline: got udp\n";
+				my $msg;
+				my $raddr = $self->{'usock'}->recv($msg, 1500);
+				my($port, $ipaddr) = sockaddr_in($raddr);
+				my $hishost = inet_ntoa($ipaddr);
+				warn "got udp from $hishost $port: $msg\n";
+				return $msg;
+			}
+		}
+		
+		if (time() > $end_t) {
+			#warn "getline: timeout\n";
+			return undef;
+		}
+	}
+}
+
+sub sendline($$)
+{
+	my($self, $line) = @_;
+	
+	warn "udp sendline: $line\n";
+	
+	return undef if ($self->{'state'} ne 'connected');
+	
+	warn "sending\n";
+	$self->{'usock'}->send($line, 0, $self->{'dest'});
+	
+	warn "sent, returning 1\n";
+	return 1;
+}
+
+
+1;
---
+++ tests/t/61udp-peer.t
@@ -0,0 +1,79 @@
+
+#
+# Test UDP core peers. While at it, check that the
+# basic loop prevention rules work.
+#
+# 1) Traffic from upstreams goes to clients.
+# 2) Traffic from core peers goes to clients.
+# 3) Traffic does not pass between peers and upstreams.
+# 4) Traffic from clients goes to core peers and upstreams.
+#
+# The testing order is selected so that the last packet proves
+# the previous ones were not transitted to the wrong sockets.
+#
+
+use Test;
+BEGIN { plan tests => 9 + 4 + 2 };
+use runproduct;
+use istest;
+use Ham::APRS::IS;
+use Ham::APRS::IS_Fake;
+use Ham::APRS::IS_Fake_UDP;
+use Time::HiRes qw(sleep);
+
+my $p = new runproduct('basic');
+
+my $upstream_call = 'FAKEUP';
+my $iss1 = new Ham::APRS::IS_Fake('127.0.0.1:10153', $upstream_call);
+ok(defined $iss1, 1, "Test failed to initialize listening server socket (IPv4)");
+$iss1->bind_and_listen();
+
+ok(defined $p, 1, "Failed to initialize product runner");
+ok($p->start(), 1, "Failed to start product");
+
+my $login = "N5CAL-1";
+my $server_call = "TESTING";
+my $client = new Ham::APRS::IS("localhost:55152", $login);
+ok(defined $client, 1, "Failed to initialize Ham::APRS::IS");
+
+my $ret;
+$ret = $client->connect('retryuntil' => 8);
+ok($ret, 1, "Failed to connect to the server: " . $client->{'error'});
+
+my $is1 = $iss1->accept();
+ok(defined $is1, (1), "Failed to accept connection 1 from server");
+ok($iss1->process_login($is1), 'ok', "Failed to accept login 1 from server");
+
+my $udp = new Ham::APRS::IS_Fake_UDP('127.0.0.1:16405', 'N0UDP');
+ok(defined $udp, (1), "Failed to set up UDP server socket");
+ok($udp->bind_and_listen(), 1, "Failed to bind UDP server socket");
+$udp->set_destination('127.0.0.1:16404');
+
+# test ###########################
+
+my $s;
+
+# 1) from upstream to client
+$s = "SRC>DST,qAR,IGATE:upstream to client";
+istest::txrx(\&ok, $is1, $client, $s, $s);
+
+# 2) from core peer to client
+$s = "SRC>DST,qAR,IGATE:core peer to client";
+istest::txrx(\&ok, $udp, $client, $s, $s);
+
+# 4) from client to peers and upstreams
+$s = "SRC>DST,qAR,IGATE:from client";
+istest::txrx(\&ok, $client, $is1, $s, $s);
+
+my $r = $udp->getline();
+ok($r, $s, "Failed to pass packet from client to UDP peer");
+
+# disconnect ####################
+
+$ret = $client->disconnect();
+ok($ret, 1, "Failed to disconnect from the server: " . $client->{'error'});
+
+# stop
+
+ok($p->stop(), 1, "Failed to stop product");
+

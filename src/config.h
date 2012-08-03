1fd18c2cbcf4ecac817fa827edc4c07ff13dc53a
Heikki Hannikainen <hessu@hes.iki.fi>
2012-08-03 20:17:19 +0000

    Bump version number to 0.3.0
    git-svn-id: http://repo.ham.fi/svn/aprsc/trunk@583 3ce903b1-3385-4e86-93cd-f9a4a239f7ac

--- src/config.h
+++ src/config.h
@@ -12,7 +12,7 @@
 #define CONFIG_H
 
 #define PROGNAME "aprsc"
-#define VERSION "0.2.10"
+#define VERSION "0.3.0"
 #define VERSTR  PROGNAME " v" VERSION
 #define SERVERID PROGNAME " " VERSION
 #define CRLF "\r\n"

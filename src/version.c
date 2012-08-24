0736ef28bfda93d32ec0caeb614e5a9612e6bd49
Matti Aarnio <oh2mqk@sral.fi>
2012-08-24 23:38:57 +0000

    CentOS package building tests, development environment fixes, version information extraction fixes
    git-svn-id: http://repo.ham.fi/svn/aprsc/trunk@708 3ce903b1-3385-4e86-93cd-f9a4a239f7ac

--- src/version.c
+++ src/version.c
@@ -4,13 +4,9 @@
 #include "version_branch.h"
 #include "config.h"
 
-#define VERSION_BUILD VERSION "-" SVNVERSION VERSION_BRANCH
-#define VERSTR PROGNAME " " VERSION_BUILD
-#define VERSTR_APRSIS VERSTR
-
-const char version_build[] = VERSION_BUILD;
-const char verstr[] = VERSTR;
-const char verstr_aprsis[] = VERSTR_APRSIS;
+const char version_build[] = VERSION "-" SVNVERSION VERSION_BRANCH;
+const char verstr[]        = PROGNAME " " VERSION "-" SVNVERSION VERSION_BRANCH;
+const char verstr_aprsis[] = PROGNAME " " VERSION "-" SVNVERSION VERSION_BRANCH;
 
 const char verstr_build_time[] = BUILD_TIME;
 const char verstr_build_user[] = BUILD_USER;

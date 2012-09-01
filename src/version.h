12a1425b63798cf6e1d23fc68c7939cfd8b46cdb
Heikki Hannikainen <hessu@hes.iki.fi>
2012-09-01 11:38:18 +0000

    version 0.7.9
    git-svn-id: http://repo.ham.fi/svn/aprsc/trunk@794 3ce903b1-3385-4e86-93cd-f9a4a239f7ac

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * If you're making modifications, put your own variant version
  * identification in version_branch.h. Thanks!
  */
-#define VERSION "0.7.8"
+#define VERSION "0.7.9"
 
 #define APRSC_TOCALL "APSC07"
 

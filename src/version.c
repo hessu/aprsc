c2a0f5bf8a05f902b745c7cdb9aee0eb7444199d
Heikki Hannikainen <hessu@hes.iki.fi>
2012-09-18 19:16:40 +0300

    Implemented building and version control information collection for git instead of svn

--- src/version.c
+++ src/version.c
@@ -4,9 +4,9 @@
 #include "version_branch.h"
 #include "config.h"
 
-const char version_build[] = VERSION "-" SVNVERSION VERSION_BRANCH;
-const char verstr[]        = PROGNAME " " VERSION "-" SVNVERSION VERSION_BRANCH;
-const char verstr_aprsis[] = PROGNAME " " VERSION "-" SVNVERSION VERSION_BRANCH;
+const char version_build[] = VERSION "-" SRCVERSION VERSION_BRANCH;
+const char verstr[]        = PROGNAME " " VERSION "-" SRCVERSION VERSION_BRANCH;
+const char verstr_aprsis[] = PROGNAME " " VERSION "-" SRCVERSION VERSION_BRANCH;
 
 const char verstr_build_time[] = BUILD_TIME;
 const char verstr_build_user[] = BUILD_USER;

4065a98bd5d0d5328e2dafec39554447774e92b5
Heikki Hannikainen <hessu@hes.iki.fi>
2012-09-19 10:47:41 +0300

    version 0.8.1 (no changes, packaging test)

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * If you're making modifications, put your own variant version
  * identification in version_branch.h. Thanks!
  */
-#define VERSION "0.8.0"
+#define VERSION "0.8.1"
 
 #define APRSC_TOCALL "APSC08"
 

27e6b29ace11a6fb6904b20f12a407852d537034
Heikki Hannikainen <hessu@hes.iki.fi>
2012-10-01 20:29:04 +0300

    version 1.1.0

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * If you're making modifications, put your own variant version
  * identification in version_branch.h. Thanks!
  */
-#define VERSION "1.0.4"
+#define VERSION "1.1.0"
 
 #define APRSC_TOCALL "APSC10"
 

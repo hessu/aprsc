8a6611e90c8b41e131c75a2ab39f3b777c13fd84
Heikki Hannikainen <hessu@hes.iki.fi>
2012-10-21 20:53:22 +0300

    version 1.5.1

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * If you're making modifications, put your own variant version
  * identification in version_branch.h. Thanks!
  */
-#define VERSION "1.5.0"
+#define VERSION "1.5.1"
 
 #define APRSC_TOCALL "APSC15"
 

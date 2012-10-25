1ded25f96e49de440a6806a9668c8f2b820aef93
Heikki Hannikainen <hessu@hes.iki.fi>
2012-10-26 00:57:24 +0300

    t2acl: Added comments. Delete temporary file.

--- src/tools/t2acl.pl
+++ src/tools/t2acl.pl
@@ -23,6 +23,7 @@ my $url = "http://www.aprs2.net/t2servers.php";
 # If generated ACL has less than N rows, something went wrong.
 my $rows_min = 90;
 
+# IPv6 address validation (and compression)
 sub isip6 {
 	# no doubt, this could be done more cleanly and much faster
 	if (($_[0] =~ /:::/) || ($_[0] !~ /^[0-9A-Fa-f:]+$/)) { return ""; }
@@ -51,6 +52,7 @@ sub isip6 {
 	return $s;
 }                                                                                                                                                                                                                                                        
 
+# Download the current ACL
 my $ua = LWP::UserAgent->new(timeout => 5);
 my $res = $ua->request(HTTP::Request->new('GET', $url));
 
@@ -63,6 +65,7 @@ my $new_acl = $res->content;
 
 #warn "new: $new_acl\n";
 
+# Parse and validate file
 my $acl;
 my $rows = 0;
 foreach my $a (split(/[\r\n]+/, $new_acl)) {
@@ -83,6 +86,8 @@ foreach my $a (split(/[\r\n]+/, $new_acl)) {
 		next;
 	}
 	
+	# hostname? Currently there's a single hostname in the ACL,
+	# which needs to be resolved.
 	if ($a =~ /^([A-Za-z][A-Za-z0-9-]*[A-Za-z0-9]\.)+[A-Za-z]{2,}$/) {
 		#warn "lookup: $a\n";
 		my $packed = gethostbyname($a);
@@ -103,21 +108,27 @@ foreach my $a (split(/[\r\n]+/, $new_acl)) {
 
 #warn $acl;
 
+# If the ACL is way too small, bail out just in case.
 if ($rows < $rows_min) {
 	die "T2 ACL generated with less than $rows_min rows: $rows - suspecting failure, aborting\n";
 }
 
+# Write out to temporary file
 my $acl_tmp = "$acl_file.tmp";
 open(ACL, ">$acl_tmp") || die "Failed to open $acl_tmp for writing: $!\n";
 print ACL $acl || die "Failed to write $acl_tmp: $!\n";
 close(ACL) || die "Failed to close $acl_tmp after writing: $!\n";
 
+# Compare, and exit if they are equal
 if (-f $acl_file && compare($acl_tmp, $acl_file) == 0) {
+	unlink($acl_tmp) || die "Failed to remove $acl_tmp: $!\n";
 	exit(0);
 }
 
+# Install new version
 rename($acl_tmp, $acl_file) || die "Failed to rename $acl_tmp to $acl_file: $!\n";
 
+# Tell aprsc to reload by sending SIGUSR1
 open(PID, $pid_file) || die "Failed to open $pid_file for reading: $!\n";
 my $pid = <PID>;
 close(PID) || die "Failed to close $pid_file after reading: $!\n";
@@ -138,4 +149,5 @@ foreach my $name (split(' ', $Config{sig_name})) {
 if (kill($signo{'USR1'}, $pid) != 1) {
 	die "T2 ACL reload: Failed to reload aprsc <$pid> configuration with signal $signo{USR1}\n";
 }
-#warn "ok: " . join("\n", @acl) . "\n";
+
+#warn "ok";

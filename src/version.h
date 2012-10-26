b51c3707c13b96161c5be51135b0356b7fcaba32
Heikki Hannikainen <hessu@hes.iki.fi>
2012-10-26 22:20:52 +0300

    version 1.6.6

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * If you're making modifications, put your own variant version
  * identification in version_branch.h. Thanks!
  */
-#define VERSION "1.6.5"
+#define VERSION "1.6.6"
 
 #define APRSC_TOCALL "APSC16"
 

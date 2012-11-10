0f3bdbf8cf50e8e0e665300a15c740e4707ea929
Heikki Hannikainen <hessu@hes.iki.fi>
2012-11-10 12:51:26 +0200

    version 1.7.3

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * If you're making modifications, put your own variant version
  * identification in version_branch.h. Thanks!
  */
-#define VERSION "1.7.2"
+#define VERSION "1.7.3"
 
 #define APRSC_TOCALL "APSC17"
 

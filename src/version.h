898e4b537b6c7f3ed8e6ac5791660f3f74ebf3b0
Heikki Hannikainen <hessu@hes.iki.fi>
2013-04-24 07:54:01 +0300

    version 2.0.3

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * If you're making modifications, put your own variant version
  * identification in version_branch.h. Thanks!
  */
-#define VERSION "2.0.2"
+#define VERSION "2.0.3"
 
 #define APRSC_TOCALL "APSC20"
 

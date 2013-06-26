9e7f488cb9722854a684e27d8f4b08ac2c58bf17
Heikki Hannikainen <hessu@hes.iki.fi>
2013-06-27 00:54:30 +0300

    aprsis2: write header and tail around outgoing message

--- src/aprsis2.c
+++ src/aprsis2.c
@@ -5,6 +5,22 @@
 #include "aprsis2.pb-c.h"
 #include "version.h"
 
+#define IS2_HEAD_LEN 4
+#define IS2_TAIL_LEN 1
+
+static void *is2_allocate_buffer(int len)
+{
+	int nlen = len + IS2_HEAD_LEN + IS2_TAIL_LEN;
+	char *buf = hmalloc(nlen);
+	uint32_t *ip = (uint32_t *)buf;
+	
+	*ip = htonl(len);
+	
+	buf[nlen-1] = '\n';
+	
+	return (void *)buf;
+}
+
 int is2_out_server_signature(struct worker_t *self, struct client_t *c)
 {
 	ServerSignature sig = SERVER_SIGNATURE__INIT;
@@ -14,17 +30,17 @@ int is2_out_server_signature(struct worker_t *self, struct client_t *c)
 	sig.app_name = verstr_progname;
 	sig.app_version = version_build;
 	len = server_signature__get_packed_size(&sig);
-	buf = hmalloc(len);
-	server_signature__pack(&sig, buf);
+	buf = is2_allocate_buffer(len);
+	server_signature__pack(&sig, buf + IS2_HEAD_LEN);
 	
-	c->write(self, c, buf, len);
+	c->write(self, c, buf, len + IS2_HEAD_LEN + IS2_TAIL_LEN);
 	
 	hfree(buf);
 	
 	return 0;
 }
 
-int is2_in_server_signature(struct worker_t *self, struct client_t *c)
+int is2_data_in(struct worker_t *self, struct client_t *c)
 {
 	return 0;
 }

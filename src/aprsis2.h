746c8a0cf089180967474545055a0ae0551c79e3
Heikki Hannikainen <hessu@hes.iki.fi>
2013-06-28 08:40:53 +0300

    IS2: implement ping reply

--- src/aprsis2.h
+++ src/aprsis2.h
@@ -5,7 +5,6 @@
 #include "worker.h"
 
 extern int is2_out_server_signature(struct worker_t *self, struct client_t *c);
-extern int is2_in_server_signature(struct worker_t *self, struct client_t *c, char *s, int len);
 extern int is2_deframe_input(struct worker_t *self, struct client_t *c, int start_at);
 
 extern int is2_out_ping(struct worker_t *self, struct client_t *c);
